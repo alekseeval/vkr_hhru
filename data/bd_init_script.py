dde03094efe07b8ee611c5aeafa4b9877e3d878c
alekseeval <alekseeval2000@mail.ru>
2021-05-14 22:51:38 +0800

    Добавлен актуальный бэкап схемы БД

--- data/bd_init_script.py
+++ data/bd_init_script.py
@@ -1,7 +1,7 @@
 from services.db_service import DbService
 
 db_service = DbService()
-with open('..//data/hh_ru_backup_09052021_schema') as file:
+with open('..//data/hh_ru_backup_14052021_schema') as file:
     db_service.execute_file_script(file)
 
 # Удаление использованных данных
