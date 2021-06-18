3df632278cf6b8e2f0930c0b36ff883cfc662bad
alekseeval <alekseeval2000@mail.ru>
2021-06-19 03:40:50 +0800

    Изменен файл из которого выгружается бэкап базы данных

--- data/bd_init_script.py
+++ data/bd_init_script.py
@@ -1,7 +1,7 @@
 from services.db_service import DbService
 
 db_service = DbService()
-with open('..//data/hh_ru_backup_14052021_schema') as file:
+with open('hh_ru_backup_current_schema') as file:
     db_service.execute_file_script(file)
 
 # Удаление использованных данных
