d39295c7c3c8889d0bf08b4bf6e3f887336ff301
alekseeval <alekseeval2000@mail.ru>
2021-06-25 00:01:51 +0800

    Изменена структура проекта

--- data/init/bd_init_script.py
+++ data/init/bd_init_script.py
@@ -1,7 +1,7 @@
 from services.db_service import DbService
 
 db_service = DbService()
-with open('hh_ru_backup_current_schema') as file:
+with open('../hh_ru_backup_current_schema') as file:
     db_service.execute_file_script(file)
 
 # Удаление использованных данных
